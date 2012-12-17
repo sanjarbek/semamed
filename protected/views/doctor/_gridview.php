bf6dd919629b54791b06bf40237e050ae07d6b97
Sanzharbek Amatov <asanjarbek@gmail.com>
2012-12-17 16:01:21 +0600

    reversed to old bootstrap

--- protected/views/doctor/_gridview.php
+++ protected/views/doctor/_gridview.php
@@ -6,6 +6,12 @@
     'filter'=>$model,
     'template'=>'{items}{pager}{summary}',
     'columns'=>array(
+        array(
+            'name'=>'No',
+            'type'=>'raw',
+            'value'=>'$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1',
+            'filter'=>''//without filtering
+        ),
         'doctor_id',
         'doctor_fullname',
         'doctor_phone',
@@ -16,7 +22,11 @@
         ),
         array(
             'name'=>'doctor_enable',
-            'value'=>'CHtml::encode($data->getStatusText())',
+            'value'=>'CHtml::encode($data->getEnableStatus())',
+            'filter'=>array(
+                0=>Yii::t('status', 'false'),
+                1=>Yii::t('status', 'true'),
+            ),
         ),
 //		'created_at',
 //		'created_user',
